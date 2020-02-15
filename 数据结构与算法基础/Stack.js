3c0eec7f956d59f67f6320e406d09f08c407c85c
HytonightYX <whosiyuan@qq.com>
2020-02-15 08:21:58 +0800

    数据结构-Stack

---
+++ 数据结构与算法基础/Stack.js
@@ -0,0 +1,50 @@
+class Stack {
+  constructor(maxSize) {
+    // 参数不合法或者不给，给默认参数
+    if (isNaN(maxSize)) {
+      maxSize = 10
+    }
+
+    this.maxSize = maxSize
+    this.container = []
+  }
+
+  isEmpty() {
+    return this.container.length === 0
+  }
+
+  isFull(number = 0) {
+    return this.container.length === this.maxSize
+  }
+
+  push(ele) {
+    if (this.isFull()) {
+      console.log('Stack Overflow')
+      return
+    }
+    this.container.push(ele)
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      console.log('Stack Underflow')
+      return undefined
+    }
+    return this.container.pop()
+  }
+
+  /**
+   * returns the top most elements in the stack, but doesn’t delete it.
+   */
+  peek() {
+    if (this.isEmpty()) {
+      console.log('Stack Underflow')
+      return undefined
+    }
+    return this.container[this.container.length - 1];
+  }
+
+  clear() {
+    this.container = []
+  }
+}
